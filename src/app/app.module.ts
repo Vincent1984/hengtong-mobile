9f2a9b473bf22fdbaf7155677441d9ef245d5dc7
liuxiaohui <liuxiaohui1@rongcapital.cn>
2016-11-26 15:27:00 +0800

    追加定义基本的页面目录

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+//我们的项目
+import { MuseumHomePage } from '../pages/museum-home/home';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    MuseumHomePage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -23,7 +26,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    MuseumHomePage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
