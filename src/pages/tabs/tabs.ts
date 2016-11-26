24c7c049ecd7bbdff4c939c1b576473dc133dbab
liuxiaohui <liuxiaohui1@rongcapital.cn>
2016-11-26 17:19:20 +0800

    提交新tab

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -6,6 +6,7 @@ import { ThematicActivityPage } from '../thematic-activity/thematic-activity';
 import { JournalArticlesPage } from '../journal-articles/journal-articles';
 import { OthersPage } from '../others/others';
 import { MyArtMuseumPage } from '../my-art-museum/my-art-museum';
+import { MuseumHomePage } from '../museum-home/museum-home';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -14,6 +15,7 @@ export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
 
+  tab00Root: any = MuseumHomePage;
   tab01Root: any = IntangibleCulturalHeritagePage;
   tab02Root: any = CulturalForumPage;
   tab03Root: any = ThematicActivityPage;
