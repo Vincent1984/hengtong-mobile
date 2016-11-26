deb8df96197c4f8ee2176d77ec609124f1b4a246
liuxiaohui <liuxiaohui1@rongcapital.cn>
2016-11-26 15:33:30 +0800

    追加定义非遗文化

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,12 +4,15 @@ import { HomePage } from '../home/home';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 
+import { MuseumHomePage } from '../museum-home/home';
+
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
+  tab0Root: any = MuseumHomePage;
   tab1Root: any = HomePage;
   tab2Root: any = AboutPage;
   tab3Root: any = ContactPage;
