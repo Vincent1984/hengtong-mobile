71d9022c1f304084e287cd8cf363c9831a9f6032
liuxiaohui <liuxiaohui1@rongcapital.cn>
2016-11-26 17:15:13 +0800

    Merge branch 'master' of https://github.com/brotherhui/hengtong-mobile
    Conflicts:
    	www/build/main.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 //我们的项目
 import { MuseumHomePage } from '../pages/museum-home/museum-home';
@@ -17,9 +14,6 @@ import { MyArtMuseumPage } from '../pages/my-art-museum/my-art-museum';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage,
     IntangibleCulturalHeritagePage,
     MuseumHomePage,
@@ -35,10 +29,7 @@ import { MyArtMuseumPage } from '../pages/my-art-museum/my-art-museum';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     TabsPage,
-    HomePage,
     MuseumHomePage,
     IntangibleCulturalHeritagePage,
     CulturalForumPage,
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 
-import { HomePage } from '../home/home';
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-
 import { IntangibleCulturalHeritagePage } from '../intangible-cultural-heritage/intangible-cultural-heritage';
 import { CulturalForumPage } from '../cultural-forum/cultural-forum';
 import { ThematicActivityPage } from '../thematic-activity/thematic-activity';
@@ -17,9 +13,6 @@ import { MyArtMuseumPage } from '../my-art-museum/my-art-museum';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  // tab1Root: any = HomePage;
-  // tab2Root: any = AboutPage;
-  // tab3Root: any = ContactPage;
 
   tab01Root: any = IntangibleCulturalHeritagePage;
   tab02Root: any = CulturalForumPage;
