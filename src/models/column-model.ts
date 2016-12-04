d51475afc320b2ffeebe1aa9fab1c334b8369852
wuyingbo <wuyingbo@rongcapital.cn>
2016-12-04 16:32:30 +0800

    123

--- src/models/column-model.ts
+++ src/models/column-model.ts
@@ -1,19 +1,21 @@
 export class ColumnModel {
   columnType: string;
   columnId: string;
+  columnKey: string;
   columnName: string;
   subColumns: Array<ColumnModel>;
 
   constructor(public _columnType: string, public _columnId: string, _columnName: string, _subColumns: Array<ColumnModel>) {
     this.columnType = _columnType;
     this.columnId= _columnId;
+    this.columnName = _columnName;
     this.subColumns = _subColumns;
   }
 
   public static build() {
-    let sub_00_00 = new ColumnModel('FYWH', '120', '遗动态非', null);
+    let sub_00_00 = new ColumnModel('FYWH', '120', '非遗动态', null);
     let sub_00_01 = new ColumnModel('FYWH', '121', '非遗名录', null);
-    let sub_00_02 = new ColumnModel('FYWH', '122', '非遗名录', null);
+    let sub_00_02 = new ColumnModel('FYWH', '122', '非遗传承', null);
     let column_01 = new ColumnModel('FYWH', '12', '非遗文化', [sub_00_00, sub_00_01, sub_00_02]);
 
     let column_02 = new ColumnModel('WHJT', '22', '文化讲堂', null);
--- src/pages/column/column-with-tab.ts
+++ src/pages/column/column-with-tab.ts
@@ -3,16 +3,25 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, } from 'ionic-angular';
 
 @Component({
-  selector: 'column-a',
-  templateUrl: 'column-a.html'
+  selector: 'column-with-tab',
+  templateUrl: 'column-with-tab.html'
 })
-export class ColumnAPage {
+export class ColumnWithTabPage {
 
   selectedItem: any;
   icons: string[];
   items: Array<{ title: string, note: string, icon: string }>;
+  columnModel: any;
+  subColumns: any;
+  hasSubTab: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.columnModel = navParams.data;
+    
+    if (this.columnModel.subColumns) {
+      this.hasSubTab = true;
+    }
+
     this.items = [];
     for (var i = 0; i < 30; i++) {
       this.items.push({ title: 'test', note: 'test', icon: 'http://d.ifengimg.com/q75/img1.ugc.ifeng.com/newugc/20161124/image/17/201_0gZKL05e08c_watermark0gZK2bN007O.jpg' });
