6d1097b3937a31e60fa22f535766735c7455c777
ftaft2016 <ftaft2000@msn.com>
2016-12-04 11:12:03 +0800

    sass overall variable

--- src/pages/museum-home/museum-home.ts
+++ src/pages/museum-home/museum-home.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component,ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-museum-home',
@@ -11,5 +11,16 @@ export class MuseumHomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  legacy: string = "puppies";
 
+  mySlideOptions = {
+    initialSlide: 0,
+    loop: true,
+    pager:true
+  };
+  @ViewChild('mySlider') slider: Slides;
+
+  goToSlide() {
+    this.slider.slideTo(2, 500);
+  }
 }
