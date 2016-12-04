0271180b4140915491709f8fb46672c10d84b043
liuxiaohui <liuxiaohui1@rongcapital.cn>
2016-12-04 12:09:00 +0800

    Merge branch 'master' of https://github.com/brotherhui/hengtong-mobile
    Conflicts:
    	www/build/main.js

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import { Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'login-page',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  constructor() {
+  todo = {};
 
+  constructor(private formBuilder: FormBuilder) {
+    this.ionViewLoaded();
+  }
+  ionViewLoaded() {
+    this.todo = this.formBuilder.group({
+      name: ['', Validators.required],
+      password: [''],
+    });
+  }
+  loginForm(){
+    console.log(this.todo.toString())
   }
 }
