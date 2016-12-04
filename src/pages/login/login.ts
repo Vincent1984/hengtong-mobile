98dfedfd24cf0b878834de9c41c137b8b9082205
liuxiaohui <liuxiaohui1@rongcapital.cn>
2016-12-04 12:27:15 +0800

    Merge branch 'master' of https://github.com/brotherhui/hengtong-mobile
    Conflicts:
    	www/build/main.js

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
+
+import { Nav } from 'ionic-angular';
+import { RegistPage } from '../regist/regist';
 
 @Component({
   selector: 'login-page',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  todo = {};
+
+  @ViewChild(Nav) nav: Nav;
+
+  registPage: any = RegistPage;
+
+  todo = {};R
 
   constructor(private formBuilder: FormBuilder) {
     this.ionViewLoaded();
@@ -19,5 +27,9 @@ export class LoginPage {
   }
   loginForm(){
     console.log(this.todo.toString())
+  }
+  openRegist(){
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(this.registPage);
   }
 }
