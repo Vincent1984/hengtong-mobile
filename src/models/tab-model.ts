a8b5e8ea77553c7f7098d09d85cac874ed323fa9
wuyingbo <wuyingbo@rongcapital.cn>
2016-12-09 02:07:50 +0800

    123

--- src/models/tab-model.ts
+++ src/models/tab-model.ts
@@ -16,15 +16,15 @@ export class TabModel {
     let tab_02 = new TabModel(ColumnWithTabPage, '2', '讲堂', 'tab02', ColumnInfoModel.buildJT());
     let tab_03 = new TabModel(ColumnWithTabPage, '3', '专题', 'tab03', ColumnInfoModel.buildZT());
     let tab_04 = new TabModel(ColumnWithTabPage, '4', '其他', 'tab04', ColumnInfoModel.buildQT());
-    let tab_05 = new TabModel(SubTabsPage, '5', '我的', 'tab05', ColumnInfoModel.buildWD());
+    let tab_05 = new TabModel(SubTabsPage, '5', '我的', 'tab05', null);
 
     return [tab_00, tab_01, tab_02, tab_03, tab_04, tab_05];
   }
 
   public static buildSubTabs() {
-    let tab_00 = new TabModel(ColumnWithMorePage, '1', '首页', 'tab01', ColumnInfoModel.buildSY());
-    let tab_01 = new TabModel(ColumnWithMorePage, '1', '非遗', 'tab01', ColumnInfoModel.buildFY());
-    let tab_02 = new TabModel(ColumnWithMorePage, '2', '讲堂', 'tab02', ColumnInfoModel.buildJT());
+    let tab_00 = new TabModel(ColumnWithMorePage, '0', '联系我们', 'sub-tab00', ColumnInfoModel.buildWD());
+    let tab_01 = new TabModel(ColumnWithMorePage, '1', '用户留言', 'sub-tab01', ColumnInfoModel.buildWD());
+    let tab_02 = new TabModel(ColumnWithMorePage, '2', '版本更新', 'sub-tab02', ColumnInfoModel.buildWD());
 
     return [tab_00, tab_01, tab_02];
   }
