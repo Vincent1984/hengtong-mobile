61e87aee58b4022fb2ce25251f449f2fac7e828e
liuxiaohui <liuxiaohui1@rongcapital.cn>
2016-12-10 08:38:32 +0800

    调整样式

--- src/pages/item-details/item-details.ts
+++ src/pages/item-details/item-details.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 
 import { NavController, NavParams } from 'ionic-angular';
-
+import {DynamicComponentLoader, Injector, ElementRef, OnInit} from "angular2/core";
 
 @Component({
   selector: 'item-details-page',
   templateUrl: 'item-details.html'
 })
-export class ItemDetailsPage {
+export class ItemDetailsPage{
   selectedItem: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
