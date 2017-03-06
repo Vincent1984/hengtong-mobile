05d3ffae6434c2a9d80bcfc6aa2302d8b127d1ce
wuyingbo <wuyingbo@rongcapital.cn>
2017-03-07 00:48:14 +0800

    修改服务地址

--- src/services/constants/constants.ts
+++ src/services/constants/constants.ts
@@ -1,6 +1,8 @@
 export class Constants {
 
-  public static URL_PATH = 'http://218.61.0.14:8080';   //地址
+  //public static URL_PATH = 'http://218.61.0.14:8080';   //地址
+  public static URL_PATH = 'http://dlqzysg.com:10091';
+  //public static URL_PATH = 'http://dlqzysg.com';
 
   public static LIST_URL = Constants.URL_PATH + '/dlqzysgweb/web/commonContent/list';   //获取列表的信息
 
